3d6cc08a7e50b1ecb1f03761c1147ba52b52cb63
Adriana Orellana <adriana.orellana.torrico@gmail.com>
2023-11-28 00:49:02 +0100

    Add goal detection in computer_vision.py

--- src/computer_vision.py
+++ src/computer_vision.py
@@ -85,11 +85,37 @@ def calculate_new_point(point, centroid, padding):
 
     return original_point + padding * (vector / norm)
 
+def draw_neighbours(map_img, neighbors):
+    for point in neighbors:
+        point_int = tuple(int(val) for val in point)
+        cv2.circle(map_img, point_int, 5, (0, 0, 255), -1)
+
 def draw_path(map_img, path):
     for i in range(len(path) - 1):
         start = tuple(int(val) for val in path[i])
         end = tuple(int(val) for val in path[i + 1])
         cv2.line(map_img, start, end, (0, 255, 0), 2)
+
+def detect_goal(map_img):
+    template = cv2.imread('../assets/images/goal.jpeg')
+    template = cv2.resize(template, (200, 200))
+
+    map_img_gray = cv2.cvtColor(map_img, cv2.COLOR_BGR2GRAY)
+    template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.matchTemplate(map_img_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.7
+    loc = np.where(res >= threshold)
+
+    w, h = template_gray.shape[::-1]
+    for pt in zip(*loc[::-1]):
+        cv2.circle(map_img, (pt[0] + w // 2, pt[1] + h // 2), 3, (255, 0, 0), -1)
+
+    return (pt[0] + w // 2, pt[1] + h // 2)
+
+
+def draw_goal(map_img, end):
+    cv2.circle(map_img, end, 7, (255, 0, 0), -1)
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -110,43 +136,50 @@ def main():
 
         binary_img = preprocess_image(frame)
 
-        if capture_data:
-            capture_map, coord_to_transform, map_img, pts2 = capture_map_data(frame, binary_img, max_width, max_height)
-            capture_data = False
-            capture_obstacle = not capture_map
+        try:
+            if capture_data:
+                capture_map, coord_to_transform, map_img, pts2 = capture_map_data(frame, binary_img, max_width, max_height)
+                capture_data = False
+                capture_obstacle = not capture_map
 
-        if capture_map and not capture_obstacle:
-            neighbors = capture_obstacle_data(map_img, padding)
-            if neighbors:
-                start, end = neighbors[1], neighbors[6]
-                path = astar(neighbors, start, end)
+            if capture_map and not capture_obstacle:
+                neighbors = capture_obstacle_data(map_img, padding)
+                
+                if neighbors:
+                    start = neighbors[1]
+                    end = detect_goal(map_img)
 
-                print('--- Path Info ---\n', f'Path length: {len(path)}\n', neighbors, start, end, f'Path: {path}')
+                    neighbors.append(end)
+                    path = astar(neighbors, start, end)
 
-                capture_obstacle = True
-                capture_map = False
+                    print('--- Path Info ---\n', f'Path length: {len(path)}\n', neighbors, start, end, f'Path: {path}')
 
-        if capture_obstacle:
-            M = cv2.getPerspectiveTransform(coord_to_transform, pts2)
-            map_img = cv2.warpPerspective(frame, M, (max_width, max_height))
-            
-            for point in neighbors:
-                point_int = tuple(int(val) for val in point)
-                cv2.circle(map_img, point_int, 5, (0, 0, 255), -1)
+                    capture_obstacle = True
+                    capture_map = False
 
-            draw_path(map_img, path)
-            map_img = cv2.rotate(map_img, cv2.ROTATE_90_CLOCKWISE)
-            cv2.imshow('Map', map_img)
+            if capture_obstacle:
+                M = cv2.getPerspectiveTransform(coord_to_transform, pts2)
+                map_img = cv2.warpPerspective(frame, M, (max_width, max_height))
 
-        cv2.imshow('Original image', frame)
-        key = cv2.waitKey(24)
+                draw_neighbours(map_img, neighbors)
+                draw_path(map_img, path)
+                draw_goal(map_img, end)
 
-        if key == ord('q'):
-            print("Quitting...")
-            break
-        elif key == ord('p'):
-            print("Capturing map...")
-            capture_data, capture_map, capture_obstacle = True, False, False
+                map_img = cv2.rotate(map_img, cv2.ROTATE_90_CLOCKWISE)
+                cv2.imshow('Map', map_img)
+
+            cv2.imshow('Original image', frame)
+            key = cv2.waitKey(24)
+
+            if key == ord('q'):
+                print("Quitting...")
+                break
+            elif key == ord('p'):
+                print("Capturing map...")
+                capture_data, capture_map, capture_obstacle = True, False, False
+        except Exception as e:
+            print("Error: ", e)
+            continue
 
     cap.release()
     cv2.destroyAllWindows()
