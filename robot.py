1390cfcf65730bcd9c3ce92b6a558f26de6328f4
Paulo Khayat <paulokhayat@gmail.com>
2023-11-27 11:55:47 +0100

    test 2

--- robot.py
+++ robot.py
@@ -6,14 +6,24 @@ def motors(left, right):
         "motor.right.target": [right],
     }
 
-client = ClientAsync()
+def on_variables_changed(node, variables):
+    try:
+        prox = variables["prox.horizontal"]
+        prox_front = prox[2]
+        speed = -prox_front // 10
+        node.send_set_variables(motors(speed, speed))
+    except KeyError:
+        pass  # prox.horizontal not found
 
-async def prog():
-    node = await client.wait_for_node()
-    await node.lock()
-    await node.set_variables(motors(50, 50))
-    await client.sleep(2)
-    await node.set_variables(motors(0, 0))
-    await node.unlock()
+with ClientAsync() as client:
+    async def prog():
+        with await client.lock() as node:
+
+            # Wait for reading changes
+            await node.watch(variables=True)
+            node.add_variables_changed_listener(on_variables_changed)
+            #while True:
+                
+            await client.sleep()
 
 client.run_async_program(prog)
