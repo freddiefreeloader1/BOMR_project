3b2b612e7c5c7b877533221262ae65116b62b68c
Paulo Khayat <paulokhayat@gmail.com>
2023-11-27 15:00:42 +0100

    Add documentation to robot code

--- robot.py
+++ robot.py
@@ -2,23 +2,30 @@ from tdmclient import ClientAsync
 from path_following import Robot, get_angle_to
 import math
 robot = Robot(0,0,0,[(0, 0), (1, 1),(2,1),(3,3)])
+
+### ---- HELPER FUNCTIONS FOR THYMIO ---- ###
 def motors(left, right):
     return {
         "motor.left.target": [left],
         "motor.right.target": [right],
     }
 
-def steer(client,node):
-    global robot
-    point, a = robot.path_follower.getLookaheadEdge(robot.odometry)
-    #point = [1,0]
-    angle = get_angle_to(robot.odometry,point)
-    #node.send_set_variables(motors(0,0))
-    print("ANGLE", "{:.2f}".format(angle), point, a,"ROBOT", "{:.2f}".format(robot.odometry.x), "{:.2f}".format(robot.odometry.y), "{:.2f}".format(robot.odometry.angle))
-    forward = 50
-    steer = 1.1
-    node.send_set_variables(motors(int(-steer*forward*angle)+forward, int(steer*forward*angle)+forward))
+### ---- ROBOT CODE ---- ###
 
+#Steer the robot to a point
+def steer(node, robot ,point):
+    angle = get_angle_to(robot.odometry,point)
+    
+    print("TARGET: {:.2f}, {} ROBOT: {:.2f}, {:,2f} angle - {:.2f}".format(angle,point,robot.odometry.x,robot.odometry.y,robot.odometry.angle))
+    # SPEED CONSTANTS
+    forward_speed = 100
+    steer_gain = 70
+    steer_max = 70
+
+    steer = steer_gain * angle
+    node.send_set_variables(motors(int(-steer + forward_speed ), int( steer + forward_speed)))
+
+# Async sensor reading update
 def on_variables_changed(node, variables):
     try:
         #TODO: proximity values
@@ -37,11 +44,12 @@ def on_variables_changed(node, variables):
         except KeyError:
             right_speed = 0
     
-        #print(robot.odometry.x, robot.odometry.y, robot.odometry.angle)
+        #ODOMETRY CONSTANTS
         robot_diameter_m = 0.1
         speed_to_m = 0.01
         motor_read_freq = 100
 
+        #Update Odometry:
         robot.odometry.x += (math.cos(robot.odometry.angle)*speed_to_m*(left_speed + right_speed)/2)/motor_read_freq
         robot.odometry.y += (math.sin(robot.odometry.angle)*speed_to_m*(left_speed + right_speed)/2)/motor_read_freq #speed in cm to robot pos in meters
         dtheta = (right_speed - left_speed) * speed_to_m / robot_diameter_m
@@ -51,11 +59,18 @@ def on_variables_changed(node, variables):
 
 with ClientAsync() as client:
     async def prog():
+        global robot
         with await client.lock() as node:
+            #Set up listener functions
             await node.watch(variables=True)
             node.add_variables_changed_listener(on_variables_changed)
             while True:
-                steer(client,node)
+                # path follow loop:
+
+                point = await robot.path_follower.getLookaheadEdge(robot.odometry)
+
+                steer(node, robot, point)
+
                 await client.sleep(0.05)
     
     client.run_async_program(prog)
