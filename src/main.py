4c282f13c7e3527388835c1676657ce165349858
Paulo Khayat <paulokhayat@gmail.com>
2023-12-04 00:30:41 +0100

    do path copy

--- src/main.py
+++ src/main.py
@@ -5,13 +5,17 @@ from camera_main import CameraClose, CameraInit, CameraLoop
 from robot_main import RobotClose, RobotInit, RobotLoop
 
 def main():
+    global robot, metric_path
     CameraInit()
     RobotInit()
     # replace loop with return
     try:
         while True:
             CameraLoop()
-            RobotLoop()
+            if(robot.path_follower.path != metric_path):
+                robot.path_follower.path = metric_path
+            if(len(metric_path) > 0):
+                RobotLoop()
     except SystemExit:
         pass
     CameraClose()
