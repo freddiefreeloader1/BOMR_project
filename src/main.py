a89732343b68a16f13aac8da6ddc4624e693d88c
Paulo Khayat <paulokhayat@gmail.com>
2023-12-03 21:40:06 +0100

    Create robot loop shells

--- src/main.py
+++ src/main.py
@@ -2,19 +2,23 @@ from computer_vision import *
 from Astar_coord import *
 from Astar import * 
 from camera_main import CameraClose, CameraInit, CameraLoop
+from robot_main import RobotClose, RobotInit, RobotLoop
 
 class Quit(SystemExit):
     pass
 
 def main():
     CameraInit()
+    RobotInit()
     # replace loop with return
     try:
         while True:
             CameraLoop()
+            RobotLoop()
     except Quit:
         pass
     CameraClose()
+    RobotClose()
 
 if __name__ == "__main__":
     main()
---
+++ src/robot_main.py
@@ -0,0 +1,10 @@
+from main import Quit
+
+def RobotInit():
+    pass
+
+def RobotLoop():
+    pass
+
+def RobotClose():
+    pass
