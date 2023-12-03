5093b65a84c9501cde9935d208737d66b3d0857f
Paulo Khayat <paulokhayat@gmail.com>
2023-12-03 22:54:46 +0100

    Move loop to robot_main

--- robot.py
+++ robot.py
@@ -150,33 +150,3 @@ def on_variables_changed(node, variables):
     except KeyError:
         pass 
 
-with ClientAsync() as client:
-    async def prog():
-        global robot
-        with await client.lock() as node:
-            #Set up listener functions
-            await node.watch(variables=True)
-            node.add_variables_changed_listener(on_variables_changed)
-            while True:
-                # Update odometry:
-                robot.update_odometry()
-
-                # path follow loop:
-
-                point, _ = robot.path_follower.getLookaheadEdge(robot.odometry)
-                if(robot.state == RobotState.FOLLOWING_PATH):
-                    steer(node, robot, point)
-                elif(robot.state == RobotState.AVOIDING_WALLS):
-                    steer_danger(node,robot)
-                elif(robot.state == RobotState.STOPPED):
-                    node.send_set_variables(motors(0,0))
-                robot.state_timer -= 1
-                if(robot.state_timer < 0 and robot.state != RobotState.STOPPED):
-                    robot.state == RobotState.FOLLOWING_PATH
-                
-                if(robot.path_follower.current_edge >= len(robot.path_follower.path)-1):
-                    await node.set_variables(motors(0,0))
-                    break
-                await client.sleep(0.05)
-    
-    client.run_async_program(prog)
--- src/robot_main.py
+++ src/robot_main.py
@@ -11,34 +11,40 @@ import math
 
 
 robot = Robot(0,0,0,[(0, 0), (1, 0),(1,1),(3,1)])
+client = None
+node = None
 
-with ClientAsync() as client:
-    async def prog():
-        global robot
-        with await client.lock() as node:
-            #Set up listener functions
-            await node.watch(variables=True)
-            node.add_variables_changed_listener(on_variables_changed)
-            while True:
-                # Update odometry:
-                robot.update_odometry()
+def RobotInit():
+    global client,node
+    client = ClientAsync()
+    node = client.lock()
 
-                # path follow loop:
+    #Set up listener functions
+    node.watch(variables=True)
+    node.add_variables_changed_listener(on_variables_changed)
 
-                point, _ = robot.path_follower.getLookaheadEdge(robot.odometry)
-                if(robot.state == RobotState.FOLLOWING_PATH):
-                    steer(node, robot, point)
-                elif(robot.state == RobotState.AVOIDING_WALLS):
-                    steer_danger(node,robot)
-                elif(robot.state == RobotState.STOPPED):
-                    node.send_set_variables(motors(0,0))
-                robot.state_timer -= 1
-                if(robot.state_timer < 0 and robot.state != RobotState.STOPPED):
-                    robot.state == RobotState.FOLLOWING_PATH
-                
-                if(robot.path_follower.current_edge >= len(robot.path_follower.path)-1):
-                    await node.set_variables(motors(0,0))
-                    break
-                await client.sleep(0.05)
+def RobotLoop():
+    global robot,client,node
+# Update odometry:
+    robot.update_odometry()
+
+    # path follow loop:
+
+    point, _ = robot.path_follower.getLookaheadEdge(robot.odometry)
+    if(robot.state == RobotState.FOLLOWING_PATH):
+        steer(node, robot, point)
+    elif(robot.state == RobotState.AVOIDING_WALLS):
+        steer_danger(node,robot)
+    elif(robot.state == RobotState.STOPPED):
+        node.send_set_variables(motors(0,0))
+    robot.state_timer -= 1
+    if(robot.state_timer < 0 and robot.state != RobotState.STOPPED):
+        robot.state == RobotState.FOLLOWING_PATH
     
-    client.run_async_program(prog)
+    if(robot.path_follower.current_edge >= len(robot.path_follower.path)-1):
+        node.set_variables(motors(0,0))
+        raise Quit
+
+def RobotClose():
+    global node
+    node.unlock()
