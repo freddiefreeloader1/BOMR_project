53ca1c3fff72ef269473bb28af0d0961b9f2cc0c
Paulo Khayat <paulokhayat@gmail.com>
2023-12-04 18:54:37 +0100

    organize main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,43 @@
 from computer_vision import *
+from camera_main import thymio_angle, thymio_position, metric_path
+from robot_main import robot
 from Astar_coord import *
 from Astar import * 
 from camera_main import CameraClose, CameraInit, CameraLoop
 from robot_main import RobotClose, RobotInit, RobotLoop
+from common import Quit
+
+# This is a simple function that hard sets the robots data.
+# TODO: initialize the kalman after this step, perhaps create a 'RobotStart(pos)'
+def update_robot_odometry(robot,path,pos,angle):
+    robot.path_follower.path = path
+    robot.odometry.x = pos[0]
+    robot.odometry.y = pos[1]
+    robot.odometry.angle = angle
 
 def main():
-    global robot, metric_path
+    global robot, metric_path, thymio_position, thymio_angle
+
     CameraInit()
     RobotInit()
     # replace loop with return
     try:
         while True:
+            # Run the camera loop
             CameraLoop()
+
+            # If the camera has data for the robot, update it.
             if(robot.path_follower.path != metric_path):
-                robot.path_follower.path = metric_path
+                update_robot_odometry(robot,metric_path,thymio_position,thymio_angle)
+
+            # If the robot was given a path, start running.
             if(len(metric_path) > 0):
                 RobotLoop()
-    except SystemExit:
+
+    except Quit:
         pass
+
+    # A quit has been called (unlock that thymio!!!!)
     CameraClose()
     RobotClose()
 
