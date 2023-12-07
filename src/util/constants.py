e0b298174390828a7a21af902d726fa2af50380e
Paulo Khayat <paulokhayat@gmail.com>
2023-12-07 20:34:16 +0100

    Increase turn amount

--- src/util/constants.py
+++ src/util/constants.py
@@ -21,8 +21,8 @@ ROBOT_FOLLOW_STEER_AMOUNT = 150
 
 #avoiding constants
 ROBOT_AVOID_FORWARD_SPEED = 200
-ROBOT_AVOID_SENSOR_GAIN = 12    #how much speed do i gain for avoiding an obstacle
-ROBOT_AVOID_SENSOR_RESCIND = 4  #amount the sensor steers away the opposite wheel
+ROBOT_AVOID_SENSOR_GAIN = 6    #how much speed do i gain for avoiding an obstacle
+ROBOT_AVOID_SENSOR_RESCIND = 12  #amount the sensor steers away the opposite wheel
 ROBOT_AVOID_SENSOR_STOP = 15    #how much does the central sensor reverse the robot
 
 ROBOT_AVOID_TOUCHING_WALL = 100
